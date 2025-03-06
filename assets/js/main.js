1c5c48d629fbe53520eeda00169292d92a213a93
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2025-03-06 16:48:07 -0300

    finalizando home section e iniciando script de interação scroll header

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,10 +32,24 @@
 
 
 /*=============== HOME SWIPER ===============*/
-
+var homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader() {
+    const header = document.getElementById('header')
+    //Quando a rolagem for maior que 50 viewpor altura, 
+    //adicione a classe scroll-header à tag header
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); 
+    else header.classList.remove('scroll-header')
+  }
+  window.addEventListener('scroll', scrollHeader)
 
 /*=============== NEW SWIPER ===============*/
 
